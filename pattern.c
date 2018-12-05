e4ab10084fc268c1d34ea6a5729c2f345aed3c90
pranavCSE35 <45621478+pranavCSE35@users.noreply.github.com>
2018-12-05 14:31:40 +0530

    Update pattern.c

--- pattern.c
+++ pattern.c
@@ -3,33 +3,37 @@ int main()
 {
 	printf("Enter a number");
 	int n;
-	scanf("%d",&n);
+	scanf("%d",&n);//this denotes the number of lines to be printed
 	int i=0,j,k;
-	for(i=0;i<n;i++)
+	for(i=0;i<n;i++)//this loop is for printing the upper traingle
 	{
 		for(j=i;j<n;j++)
 		{
 			printf(" ");
 		}
-		for(j=i;j>=0;j--)
+		for(j=i;j>=0;j--)//left triangle
 		{
 			printf("*");
 		}
-		for(j=i;j>0;j--)
+		for(j=i;j>0;j--)//right triangle
 			printf("*");
 		printf("\n");
 	}
-	for(i=n-2;i>=0;i--)
+
+
+
+
+	for(i=n-2;i>=0;i--)//this loop is for printing lower triangle
 	{
 		for(j=i;j<n;j++)
 		{
 			printf(" ");
 		}
-		for(j=i;j>=0;j--)
+		for(j=i;j>=0;j--)//left triangle
 		{
 			printf("*");
 		}
-		for(j=i;j>0;j--)
+		for(j=i;j>0;j--)//right triangle
 			printf("*");
 		printf("\n");
 	}
